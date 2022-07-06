039e350cc25b325ac23f41699e95d3e9f7d4e7d7
cibs84 <d.tilesi@gmail.com>
2022-07-06 16:37:30 +0200

    MyHeader component completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
+<!-- Descrizione:
+Create un nuovo progetto utilizzando Vue CLI e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone. -->
+
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader/>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyHeader
   }
 }
 </script>
 
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './styles/common';
+
+body {
+  font-family: 'Encode Sans Condensed', sans-serif;
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,118 @@
+<template>
+    <header class="container">
+        <!-- Header - Logo -->
+        <div class="logo">
+            <a href="#">
+                <img src="../assets/img/dc-logo.png" alt="DC Logo">
+            </a>
+        </div>
+
+        <!-- Header - Main Menu -->
+        <nav>
+            <ul class="main-menu">
+                <li v-for='link, index in menuLinks' :key="index">
+                    <a :href="link.url" :class="{ 'current': link.current }">{{link.text}}</a>
+                </li> 
+            </ul>
+        </nav>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: 'MyHeader',
+    data() {
+        return {
+            menuLinks: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Tv',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '../styles/variables';
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo {
+        width: 5rem;
+    }
+    .main-menu {
+        display: flex;
+
+        li {
+            margin-left: 2rem;
+
+            a {
+                padding: 3rem 0;
+                display: block;
+                text-transform: uppercase;
+                font-size: 0.85rem;
+                font-weight: 700;
+
+                &.current {
+                    color: #0282f9;
+                    border-bottom: 5px solid $brand_primary_color;
+                }
+            }
+        }
+
+    }
+}
+</style>
